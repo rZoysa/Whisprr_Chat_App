b9e243612a457398785a4ee94f45883d4a474db8
rZoysa <rithikzoysa@gmail.com>
2025-05-30 00:50:50 +0530

    Refactor navigation methods for cleaner syntax and add Register button to LoginScreen

--- lib/utils/navigation/custom_navigation.dart
+++ lib/utils/navigation/custom_navigation.dart
@@ -3,18 +3,24 @@ import 'package:flutter/material.dart';
 
 class Customnavigation {
   static void nextMaterialPage(BuildContext context, Widget widget) {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => widget,
-        ));
+    Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
+  }
+
+  static void nextMaterialPageReplace(BuildContext context, Widget widget) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => widget),
+    );
   }
 
   static void nextCupertinoPage(BuildContext context, Widget widget) {
-    Navigator.push(
-        context,
-        CupertinoPageRoute(
-          builder: (context) => widget,
-        ));
+    Navigator.push(context, CupertinoPageRoute(builder: (context) => widget));
+  }
+
+  static void nextCupertinoPageReplace(BuildContext context, Widget widget) {
+    Navigator.pushReplacement(
+      context,
+      CupertinoPageRoute(builder: (context) => widget),
+    );
   }
 }
