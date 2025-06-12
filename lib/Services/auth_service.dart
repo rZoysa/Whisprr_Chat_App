e9526a9db91e76681ffb31753804d81e24f7b027
rZoysa <rithikzoysa@gmail.com>
2025-06-12 15:19:13 +0530

    added comments

--- lib/Services/auth_service.dart
+++ lib/Services/auth_service.dart
@@ -6,8 +6,8 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  //Sign Up Method
   Future<void> signUp(String email, String password, String username) async {
-    //Sign Up Method
     UserCredential result = await _auth.createUserWithEmailAndPassword(
       email: email,
       password: password,
@@ -15,17 +15,22 @@ class AuthService {
     final uid = result.user!.uid;
 
     AppUser user = AppUser(uid: uid, email: email, username: username);
-    await _firestore.collection('users').doc(uid).set(user.toMap()); //Save user data to Firestore
+    await _firestore
+        .collection('users')
+        .doc(uid)
+        .set(user.toMap()); //Save user data to Firestore
   }
 
+  //Login Method
   Future<void> login(String email, String password) async {
-    //Login Method
     await _auth.signInWithEmailAndPassword(email: email, password: password);
   }
 
+  //Logout Method
   Future<void> logout() async {
     await _auth.signOut();
   }
 
+  //Get current user
   User? get currentUser => _auth.currentUser;
 }
