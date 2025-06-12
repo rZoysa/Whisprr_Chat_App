2e288a5a5d73da29770128d010dc5fb20daae01b
rZoysa <rithikzoysa@gmail.com>
2025-06-12 14:01:14 +0530

    feat: Integrate Firebase into the project with necessary configurations and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:whisprr/firebase_options.dart';
 import 'package:whisprr/view_models/auth_viewmodel.dart';
 import 'package:whisprr/views/landing_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => AuthViewmodel())],
