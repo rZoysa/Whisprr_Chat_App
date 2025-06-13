170ab60948314e3d08485392ba10e57b30c746e1
rZoysa <rithikzoysa@gmail.com>
2025-06-13 17:25:17 +0530

    UI Update

--- lib/views/settings_screen/setting_screen.dart
+++ lib/views/settings_screen/setting_screen.dart
@@ -27,69 +27,81 @@ class _SettingScreenState extends State<SettingScreen> {
       ),
       body: Column(
         children: [
-          TextButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: const Text('Log out of your account?'),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop(); // Close the dialog
-                        },
-                        child: const Text('Cancel'),
-                      ),
-                      TextButton(
-                        onPressed: () async {
-                          Navigator.of(context).pop(); // Close the dialog
-
-                          // Logout the user
-                          AuthResult result = await authViewmodel.logout();
-
-                          if (!context.mounted) return;
-
-                          if (result.success) {
-                            //Show success message
-                            SnackbarUtil.showSuccess(
-                              context,
-                              result.message,
-                              duration: Duration(milliseconds: 1200),
-                            );
-
-                            // Navigate to the landing screen
-                            Customnavigation.nextMaterialPageReplaceAll(
-                              context,
-                              LandingScreen(),
-                            );
-                          } else {
-                            //Show error message
-                            SnackbarUtil.showError(context, result.message);
-                          }
-                        },
-                        child: const Text(
-                          'Logout',
-                          style: TextStyle(color: Colors.red),
+          SizedBox(
+            width: double.infinity,
+            child: TextButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text('Log out of your account?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop(); // Close the dialog
+                          },
+                          child: const Text('Cancel'),
                         ),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-            style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.all<Color>(
-                Color(0xffE8EDF5),
+                        TextButton(
+                          onPressed: () async {
+                            Navigator.of(context).pop(); // Close the dialog
+
+                            // Logout the user
+                            AuthResult result = await authViewmodel.logout();
+
+                            if (!context.mounted) return;
+
+                            if (result.success) {
+                              //Show success message
+                              SnackbarUtil.showSuccess(
+                                context,
+                                result.message,
+                                duration: Duration(milliseconds: 1200),
+                              );
+
+                              // Navigate to the landing screen
+                              Customnavigation.nextMaterialPageReplaceAll(
+                                context,
+                                LandingScreen(),
+                              );
+                            } else {
+                              //Show error message
+                              SnackbarUtil.showError(context, result.message);
+                            }
+                          },
+                          child: const Text(
+                            'Logout',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(
+                  Color.fromARGB(255, 221, 225, 233),
+                ),
+                foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                padding: WidgetStateProperty.all<EdgeInsets>(
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 32.0),
+                ),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
               ),
-              foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-              padding: WidgetStateProperty.all<EdgeInsets>(
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: const Text(
+                'Logout',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            child: const Text(
-              'Logout',
-              style: TextStyle(color: Colors.red, fontSize: 16),
             ),
           ),
         ],
